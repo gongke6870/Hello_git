d30b8b5ff6726cbd9df5f755ebefe178d270a255
Administrator <aa.163.com>
2019-12-29 21:15:35 +0800

    1.0.2

--- src/hello/hellogitee.java
+++ src/hello/hellogitee.java
@@ -4,5 +4,6 @@ public class hellogitee {
     public static void main(String[] args) {
         System.out.println("hello码云");
         System.out.println("ssss");
+        System.out.println("sss");
     }
 }
